9b73659fb26a82d8573ba75341bc79477f901561
KOF <ilikekof@126.com>
2017-08-08 16:05:50 +0800

    Added Question Class and CheckAnswer Function

--- app/src/main/java/com/example/kof/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/kof/geoquiz/MainActivity.java
@@ -4,20 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mTrueButton,mFalseBtton;
+    private Button mTrueButton,mFalseButton,mNextButton;
+    private TextView mTextView;
+    private int mCurrentIndex = 0;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_mideast,false),
+            new Question(R.string.question_africa,false),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true)
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
-        mFalseBtton = (Button) findViewById(R.id.false_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mTextView = (TextView) findViewById(R.id.question_text_view);
         mTrueButton.setOnClickListener(this);
-        mFalseBtton.setOnClickListener(this);
+        mFalseButton.setOnClickListener(this);
+        mNextButton.setOnClickListener(this);
+
+        UpdateQuestionText();
     }
 
     @Override
@@ -25,13 +42,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch ( v.getId())
         {
             case R.id.true_button:
-                Toast.makeText(this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                CheckAnswer(true);
                 break;
             case R.id.false_button:
-                Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                CheckAnswer(false);
+                break;
+            case R.id.next_button:
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                UpdateQuestionText();
                 break;
             default:
                 break;
         }
+    }
+
+    private void CheckAnswer(boolean AnswerValue)
+    {
+        int MessageResId;
+        if (mQuestionBank[mCurrentIndex].isAnswerTrue() == AnswerValue){
+            MessageResId = R.string.correct_toast;
+        }
+        else {
+            MessageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this,MessageResId, Toast.LENGTH_SHORT).show();
+    }
+
+    private void UpdateQuestionText() {
+        int QuestionId = mQuestionBank[mCurrentIndex].getTextResId();
+        mTextView.setText(QuestionId);
     }
 }
---
+++ app/src/main/java/com/example/kof/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.example.kof.geoquiz;
+
+/**
+ * Created by KOF on 8/8/2017.
+ * Store Question Text and Answer
+ */
+
+public class Question {
+    private int mTextResId;
+    private  boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
